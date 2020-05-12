8165f6261c63ef58df510e8f73cf6b54fbcf4322
Robin Ho <hechu@qq.com>
2020-05-13 01:16:53 +0800

    rolling circle works fine.

--- ex20200510_rolling_circle.py
+++ ex20200510_rolling_circle.py
@@ -9,9 +9,9 @@ from manimlib.imports import *
 class RollingCircle1(Scene):
     CONFIG = {
         "color": WHITE,
-        "r1": 2,
-        "r2": 4/3,
-        "rr": 4,
+        "r1": 4.5/2,
+        "r2": 4.5/3,
+        "rr": 4.5,
     }
 
     def construct(self):
@@ -24,6 +24,7 @@ class RollingCircle1(Scene):
 
         o2 = Dot()
         circler = Circle(radius=self.rr, color=self.color)
+
         circle1 = Circle(radius=self.r1, color=self.color,
                          fill_color=YELLOW, fill_opacity=0.5)
         arrow1 = Arrow(circle1.get_center(), UP*self.r1)
@@ -50,8 +51,8 @@ class RollingCircle1(Scene):
             angle = math.radians(-360 * alpha * (self.rr-self.r1)/self.r1)
             arrow.rotate(angle=angle, about_point=dx)
 
-            o2 = Dot(circle.get_center())
-            new_group = VGroup(circle, o2, arrow)
+            dot = Dot(dx)
+            new_group = VGroup(circle, dot, arrow)
             group.become(new_group)
             return group
 
@@ -59,20 +60,14 @@ class RollingCircle1(Scene):
                   run_time=10, rate_func=double_smooth)
         self.wait(3)
 
-        circle1 = Circle(radius=self.r1, color=self.color,
-                         fill_color=YELLOW, fill_opacity=0.5)
-        dx = circle1.get_center()
-        arrow2 = Arrow(dx, dx+DOWN*self.r1)
-        o3 = Dot()
-        g2 = VGroup(circle1, o3, arrow2)
-        g2.shift(UP*(self.r1 + self.rr))
-        trans1 = Transform(g1, g2)
+        g1.generate_target()
+        g1.target.shift(UP*(self.rr))
+        g1.target.rotate(math.radians(180))
+        trans1 = MoveToTarget(g1)
         self.play(trans1)
         self.wait(3)
-        self.remove(g2)
 
         def update2(group, alpha):
-            # print(alpha)
             angle = math.radians(-360 * alpha)
             circle = Circle(radius=self.r1, color=self.color,
                             fill_color=YELLOW, fill_opacity=0.5)
@@ -84,8 +79,8 @@ class RollingCircle1(Scene):
             angle = math.radians(-360 * alpha * (self.rr+self.r1)/self.r1)
             arrow.rotate(angle=angle, about_point=dx)
 
-            o2 = Dot(circle.get_center())
-            new_group = VGroup(circle, o2, arrow)
+            dot = Dot(dx)
+            new_group = VGroup(circle, dot, arrow)
             group.become(new_group)
             return group
 
@@ -96,30 +91,29 @@ class RollingCircle1(Scene):
         circle2 = Circle(radius=self.r2, color=self.color,
                          fill_color=BLUE, fill_opacity=0.5)
         dx = circler.get_center()
-        arrow2 = Arrow(dx, dx+DOWN*self.r2)
-        o3 = Dot()
-        g2 = VGroup(circle2, o3, arrow2)
-        g2.shift(UP*(self.r2 + self.rr))
+        arrow2 = Arrow(dx, dx+UP*self.r2)
+        dot = Dot()
+        g2 = VGroup(circle2, dot, arrow2)
+        g2.shift(UP*(self.rr-self.r2))
         trans1 = Transform(g1, g2)
         self.play(trans1)
         self.wait(3)
-        self.remove(g2)
+        self.remove(g1)
 
         def update3(group, alpha):
-            # print(alpha)
-            angle = math.radians(-360 * alpha)
+            angle = math.radians(360 * alpha)
             circle = Circle(radius=self.r2, color=self.color,
                             fill_color=BLUE, fill_opacity=0.5)
-            circle.shift(UP*(self.r2+self.rr))
+            circle.shift(UP*(self.rr-self.r2))
 
             circle.rotate(angle=angle, about_point=ORIGIN)
             dx = circle.get_center()
-            arrow = Arrow(dx, dx + DOWN*self.r2)
-            angle = math.radians(-360 * alpha * (self.r2+self.rr)/self.r2)
+            arrow = Arrow(dx, dx + UP*self.r2)
+            angle = math.radians(-360 * alpha * (self.rr-self.r2)/self.r2)
             arrow.rotate(angle=angle, about_point=dx)
 
-            o2 = Dot(circle.get_center())
-            new_group = VGroup(circle, o2, arrow)
+            dot = Dot(dx)
+            new_group = VGroup(circle, dot, arrow)
             group.become(new_group)
             return group
 
@@ -131,153 +125,33 @@ class RollingCircle1(Scene):
 class RollingCircle2(Scene):
     CONFIG = {
         "color": WHITE,
-        "r1": 4/3,
-        "r2": 4,
-        "r3": 0.7,
-        "r4": 2.1,
+        "r1": 4.5/3,
+        "r2": 4.5,
     }
 
     def construct(self):
         origin = Dot()
         [txtO] = [TexMobject(X) for X in ["O"]]
         txtO.next_to(origin, DR, buff=0.1)
-        # self.add(origin, txtO)
-        self.play(FadeIn(origin), FadeIn(txtO))
-        self.wait(1)
 
-        circle1 = Circle(radius=self.r1, color=self.color,
-                         fill_color=BLUE, fill_opacity=0.5)
-        circle2 = Circle(radius=self.r2, color=self.color)
-        arrow = Arrow(UP*self.r1, DOWN*self.r1)
-
-        g1 = VGroup(circle1, arrow)
-        g1.shift(UP*(self.r1+self.r2))
-        # self.add(circle2, g1)
-        self.play(FadeIn(circle2), FadeIn(g1))
-        self.wait(1)
-
-        t1 = TexMobject("r1:r2 = 1:3")
-        t1.shift(UP*self.r2*2+RIGHT*self.r2)
-        self.play(Write(t1))
-
-        def update1(group, alpha):
-            # print(alpha)
-            angle = math.radians(-360 * alpha)
-            circle = Circle(radius=self.r1, color=self.color,
-                            fill_color=BLUE, fill_opacity=0.5)
-            circle.shift(UP*(self.r1+self.r2))
-
-            circle.rotate(angle=angle, about_point=ORIGIN)
-            dx = circle.get_center()
-            arrow = Arrow(dx+UP*self.r1, dx + DOWN*self.r1)
-            angle = math.radians(-360 * alpha * (self.r1+self.r2)/self.r1)
-            arrow.rotate(angle=angle, about_point=dx)
-
-            new_group = VGroup(circle, arrow)
-            group.become(new_group)
-            return group
-
-        self.play(UpdateFromAlphaFunc(g1, update1),
-                  run_time=10, rate_func=double_smooth)
-        self.wait(1)
-
-        circle3 = Circle(radius=self.r3, color=self.color,
-                         fill_color=BLUE, fill_opacity=0.5)
-        circle4 = Circle(radius=self.r4, color=self.color)
-        arrow = Arrow(UP*self.r3, DOWN*self.r3)
-        g2 = VGroup(circle3, arrow)
-        g2.shift(UP*(self.r3+self.r4))
-
-        trans1 = Transform(g1, g2)
-        trans2 = Transform(circle2, circle4)
-        self.play(trans1, trans2)
-        self.wait(3)
-
-        circle5 = Circle(radius=self.r3+self.r4, color=BLUE,
-                         fill_color=BLUE, fill_opacity=0)
-        circle5.rotate(angle=math.radians(90))
-        self.play(ShowCreation(circle5))
-        self.wait(3)
-
-        llen = (self.r3 + self.r4) * PI
-
-        circle6 = Circle(radius=self.r3, color=self.color,
-                         fill_color=BLUE, fill_opacity=0.5)
-        arrow = Arrow(RIGHT*self.r3, LEFT*self.r3)
-        g3 = VGroup(circle6, arrow)
-        g3.shift(UP*llen)
-
-        line1 = Line(DOWN*llen, UP*llen)
-        line1.shift(LEFT*self.r3)
-        trans1 = Transform(circle5, line1)
-        trans2 = Transform(g2, g3)
-        self.play(FadeOut(txtO), FadeOut(origin),
-                  FadeOut(circle2))
-        self.wait(1)
-        self.remove(g1)
-        self.play(trans1, trans2)
-        self.wait(1)
-
-        def update2(group, alpha):
-            # print(alpha)
-            llen = (self.r3 + self.r4) * PI * 2
-            angle = math.radians(-360 * alpha)
-            circle = Circle(radius=self.r3, color=self.color,
-                            fill_color=BLUE, fill_opacity=0.5)
-            circle.shift(UP*llen/2 + DOWN*llen*alpha)
-
-            dx = circle.get_center()
-            arrow = Arrow(dx + RIGHT*self.r3, dx + LEFT*self.r3)
-            angle = math.radians(-360 * alpha * (self.r3+self.r4)/self.r3)
-            ags = int((360 * alpha * (self.r3+self.r4)/self.r3) % 360)
-            ta = TexMobject("angle={:d}".format(ags))
-            ta.shift(UP*self.r2*2+LEFT*self.r2)
-            # print(ags)
-
-            arrow.rotate(angle=angle, about_point=dx)
-
-            new_group = VGroup(circle, arrow, ta)
-            group.become(new_group)
-            return group
-
-        self.remove(g2)
-        self.play(UpdateFromAlphaFunc(g1, update2),
-                  run_time=10, rate_func=double_smooth)
-        self.wait(6)
-
-
-class RollingCircle3(Scene):
-    CONFIG = {
-        "color": WHITE,
-        "r1": 4/3,
-        "r2": 4,
-    }
-
-    def construct(self):
-        origin = Dot()
-        [txtO] = [TexMobject(X) for X in ["O"]]
-        txtO.next_to(origin, DR, buff=0.1)
-        self.play(FadeIn(origin), FadeIn(txtO))
-        self.wait(1)
-
+        circle_r = Circle(radius=self.r2, color=self.color)
         circle1 = Circle(radius=self.r1, color=self.color,
                          fill_color=BLUE, fill_opacity=0.5)
         arrow1 = Arrow(circle1.get_center(), UP*self.r1)
         dot1 = Dot()
-        g1 = VGroup(circle1, arrow1, dot1)
+        arc1 = Arc(radius=self.r2-self.r1,
+                   color=BLUE, start_angle=np.deg2rad(90), angle=0)
+        g1 = VGroup(dot1, circle1, arrow1, arc1)
         g1.shift(UP*(self.r2-self.r1))
 
-        circle2 = Circle(radius=self.r2, color=self.color)
-
-        t1 = TexMobject("{\\LARGE r1:r2 = 1:3}")
+        t1 = TexMobject("{\\Huge r1:r2 = 1:3}")
         t1.shift(UP*self.r2*2+RIGHT*self.r2)
 
-        self.play(FadeIn(circle2), FadeIn(g1), Write(t1))
+        self.play(FadeIn(circle_r), FadeIn(g1), FadeIn(origin), FadeIn(txtO))
+        self.play(Write(t1))
         self.wait(3)
 
-        arc = None
         def update1(group, alpha):
-            # print(alpha)
             angle = math.radians(360 * alpha)
             arc = Arc(radius=self.r2-self.r1,
                       color=BLUE, start_angle=np.deg2rad(90), angle=angle)
@@ -292,41 +166,37 @@ class RollingCircle3(Scene):
             angle = math.radians(-360 * alpha * (self.r2-self.r1)/self.r1)
             arrow.rotate(angle=angle, about_point=dx)
 
-            new_group = VGroup(circle, arrow, dot, arc)
-            group.become(new_group)
+            ng = VGroup(dot, circle, arrow, arc)
+            group.become(ng)
             return group
 
         self.play(UpdateFromAlphaFunc(g1, update1),
                   run_time=10, rate_func=double_smooth)
         self.wait(1)
 
-        circle5 = Circle(radius=self.r2-self.r1, color=BLUE,
-                         fill_color=BLUE, fill_opacity=0)
-        circle5.rotate(angle=math.radians(90))
-
-        g2 = VGroup(circle1, arrow1, dot1)
-
         llen = (self.r2 - self.r1) * PI
-        circle6 = Circle(radius=self.r1, color=self.color,
-                         fill_color=BLUE, fill_opacity=0.5)
-        dot2 = Dot(circle6.get_center())
-        arrow2 = Arrow(circle6.get_center(), LEFT*self.r1)
-        g3 = VGroup(circle6, arrow2, dot2)
-        g3.shift(UP*llen)
+        g1 = VGroup(circle1, arrow1, dot1)
+        g1.generate_target()
+        g1.target.shift(UP*llen - (self.r2-self.r1) + RIGHT*self.r1*2)
+        trans1 = MoveToTarget(g1)
 
-        line1 = Line(DOWN*llen, UP*llen)
-        line1.shift(LEFT*self.r1)
-        trans1 = Transform(circle5, line1)
-        trans2 = Transform(g2, g3)
+        linex = Line(DOWN*llen, UP*llen)
+        trans2 = Transform(arc1, linex)
 
-        self.play(FadeOut(txtO), FadeOut(origin), FadeOut(circle2))
+        self.play(FadeOut(txtO), FadeOut(origin), FadeOut(circle_r))
         self.wait(1)
-        self.remove(g1)
+        self.play(trans1, trans2)
+        self.wait(1)
+        linex.generate_target()
+        linex.target.shift(LEFT*self.r1)
+        self.remove(arc1)
+        trans1 = MoveToTarget(linex)
+        trans2 = Rotate(g1, angle=math.radians(90),
+                        about_point=circle1.get_center())
         self.play(trans1, trans2)
         self.wait(1)
 
         def update2(group, alpha):
-            # print(alpha)
             llen = (self.r2 - self.r1) * PI * 2
             angle = math.radians(-360 * alpha)
             circle = Circle(radius=self.r1, color=self.color,
@@ -339,50 +209,66 @@ class RollingCircle3(Scene):
             angle = math.radians(-360 * alpha * (self.r2-self.r1)/self.r1)
             agr = int((360 * alpha * (self.r2-self.r1)/self.r1) % 360)
             agc = "{:.1f}".format((360*alpha*(self.r2-self.r1)/self.r1)/360)
-            # agr=0
-            # agc="0"
+            # agr = 0
+            # agc = "0"
             # print(agr)
-            ta=TexMobject("angle={:d}".format(agr))
-            tb=TexMobject("round={:s}".format(agc))
+            ta = TexMobject("angle={:d}".format(agr))
+            tb = TexMobject("round={:s}".format(agc))
             ta.shift(UP*self.r2*2+LEFT*self.r2)
             tb.next_to(ta, DOWN)
 
-            arrow.rotate(angle = angle, about_point = dx)
+            arrow.rotate(angle=angle, about_point=dx)
 
-            new_group=VGroup(circle, arrow, dot1, ta, tb)
-            group.become(new_group)
+            ng = VGroup(circle, arrow, dot1, ta, tb)
+            group.become(ng)
             return group
 
-        # self.remove(g2)
         self.play(UpdateFromAlphaFunc(g1, update2),
-                  run_time = 10, rate_func = double_smooth)
+                  run_time=10, rate_func=double_smooth)
         self.wait(6)
 
 
 class Test(Scene):
     def construct(self):
-        textHuge=TextMobject("{\\Huge Huge Text 012}")
-        texthuge=TextMobject("{\\huge huge Text 012.\\#!?} Text")
-        textLARGE=TextMobject("{\\LARGE LARGE Text 012.\\#!?} Text")
-        textLarge=TextMobject("{\\Large Large Text 012.\\#!?} Text")
-        textlarge=TextMobject("{\\large large Text 012.\\#!?} Text")
-        textNormal=TextMobject("{\\normalsize normal Text 012.\\#!?} Text")
-        textsmall=TextMobject("{\\small small Text 012.\\#!?} Texto normal")
-        textfootnotesize=TextMobject(
-            "{\\footnotesize footnotesize Text 012.\\#!?} Text")
-        textscriptsize=TextMobject(
-            "{\\scriptsize scriptsize Text 012.\\#!?} Text")
-        texttiny=TextMobject("{\\tiny tiny Texto 012.\\#!?} Text normal")
-        textHuge.to_edge(UP)
-        texthuge.next_to(textHuge, DOWN, buff = 0.1)
-        textLARGE.next_to(texthuge, DOWN, buff = 0.1)
-        textLarge.next_to(textLARGE, DOWN, buff = 0.1)
-        textlarge.next_to(textLarge, DOWN, buff = 0.1)
-        textNormal.next_to(textlarge, DOWN, buff = 0.1)
-        textsmall.next_to(textNormal, DOWN, buff = 0.1)
-        textfootnotesize.next_to(textsmall, DOWN, buff = 0.1)
-        textscriptsize.next_to(textfootnotesize, DOWN, buff = 0.1)
-        texttiny.next_to(textscriptsize, DOWN, buff = 0.1)
-        self.add(textHuge, texthuge, textLARGE, textLarge, textlarge,
-                 textNormal, textsmall, textfootnotesize, textscriptsize, texttiny)
-        self.wait(3)
+        a = VGroup()
+        a.foo = Circle()
+        a.bar = Rectangle()
+        a.digest_mobject_attrs()
+        a.print_family()
+        print(id(a.foo), id(a.bar))
+        self.add(a)
+        self.wait()
+
+        a.generate_target()
+        a.target.remove(a.target.foo)
+        a.target.print_family()
+        print(id(a.target.foo), id(a.target.bar))
+
+        self.play(MoveToTarget(a))
+        self.wait()
+
+        # textHuge = TextMobject("{\\Huge Huge Text 012}")
+        # texthuge = TextMobject("{\\huge huge Text 012.\\#!?} Text")
+        # textLARGE = TextMobject("{\\LARGE LARGE Text 012.\\#!?} Text")
+        # textLarge = TextMobject("{\\Large Large Text 012.\\#!?} Text")
+        # textlarge = TextMobject("{\\large large Text 012.\\#!?} Text")
+        # textNormal = TextMobject("{\\normalsize normal Text 012.\\#!?} Text")
+        # textsmall = TextMobject("{\\small small Text 012.\\#!?} Texto normal")
+        # textfootnotesize = TextMobject(
+        #     "{\\footnotesize footnotesize Text 012.\\#!?} Text")
+        # textscriptsize = TextMobject(
+        #     "{\\scriptsize scriptsize Text 012.\\#!?} Text")
+        # texttiny = TextMobject("{\\tiny tiny Texto 012.\\#!?} Text normal")
+        # textHuge.to_edge(UP)
+        # texthuge.next_to(textHuge, DOWN, buff=0.1)
+        # textLARGE.next_to(texthuge, DOWN, buff=0.1)
+        # textLarge.next_to(textLARGE, DOWN, buff=0.1)
+        # textlarge.next_to(textLarge, DOWN, buff=0.1)
+        # textNormal.next_to(textlarge, DOWN, buff=0.1)
+        # textsmall.next_to(textNormal, DOWN, buff=0.1)
+        # textfootnotesize.next_to(textsmall, DOWN, buff=0.1)
+        # textscriptsize.next_to(textfootnotesize, DOWN, buff=0.1)
+        # texttiny.next_to(textscriptsize, DOWN, buff=0.1)
+        # self.add(textHuge, texthuge, textLARGE, textLarge, textlarge,
+        #          textNormal, textsmall, textfootnotesize, textscriptsize, texttiny)
+        # self.wait(3)
