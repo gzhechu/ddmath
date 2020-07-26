abe5b90ba4a6a668154f88a85d4ed323be74c8b5
Robin Ho <hechu@qq.com>
2020-07-26 14:52:49 +0800

    add new file

---
+++ md20200620_half_of_size.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+from manimlib.imports import *
+
+# manim ddmath/md20200620_half_of_size.py HalfSize -r1280,720 -pm
+
+
+class HalfSize(Scene):
+    CONFIG = {
+        "color": WHITE,
+        "A": np.array([2, 4, 0]),
+        "B": np.array([-4.5, -2, 0]),
+        "C": np.array([5, -2, 0]),
+        "txt": 7
+    }
+
+    def construct(self):
+        origin = Dot()
+        [txtO] = [TexMobject(X) for X in ["O"]]
+        txtO.next_to(origin, DR, buff=0.1)
+        self.play(FadeIn(origin), FadeIn(txtO))
+        self.wait()
