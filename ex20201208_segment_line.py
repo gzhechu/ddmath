9c4aebcfd02b0c54a2e5df9d63f9474ea3d8bd0f
gzhechu <gzhechu@gmail.com>
2020-12-16 00:46:41 +0800

    segment line 2a almost ready.

--- ex20201208_segment_line.py
+++ ex20201208_segment_line.py
@@ -17,11 +17,8 @@ except:
 # manim ddmath/ex20201208_segment_line.py SegLine2 -r640,360 -pl
 
 """
-ffmpeg -i voice.m4a -acodec copy v.aac -y
-ffmpeg -i bg.m4a -acodec copy b.aac -y
-cat b.aac v.aac >> sound.aac -y
-ffmpeg -i sound.aac -acodec copy -bsf:a aac_adtstoasc sound.m4a -y
-ffmpeg -i SegLine1.mp4 -i sound.m4a output.mp4
+ffmpeg -i SegLine1.mp4 -i v1.m4a output.mp4 -y
+ffmpeg -i SegLine2.mp4 -i v2.m4a output.mp4 -y
 """
 
 
@@ -94,7 +91,8 @@ F是BD的中点，稍微计算一下可知CF等于X
         txtE.next_to(ptE, DOWN, buff=0.6)
         txtF.next_to(ptF, DOWN, buff=0.6)
 
-        self.play(FadeIn(lAD), FadeIn(ptA), FadeIn(ptD), FadeIn(txtA), FadeIn(txtD))
+        self.play(FadeIn(lAD), FadeIn(ptA), FadeIn(
+            ptD), FadeIn(txtA), FadeIn(txtD))
         self.wait(5)
 
         self.play(FadeIn(ptB), FadeIn(ptC), FadeIn(txtB), FadeIn(txtC))
@@ -114,7 +112,7 @@ F是BD的中点，稍微计算一下可知CF等于X
         gF.set_color(RED)
         self.play(GrowFromCenter(lBD), run_time=0.5)
         trans2 = TransformFromCopy(lBD, tx1b)
-        self.play(trans2, run_time=0.5)                        
+        self.play(trans2, run_time=0.5)
         self.remove(lBD)
         ptB.set_color(RED)
         ptD.set_color(RED)
@@ -191,24 +189,38 @@ class SegLine2(Scene):
         """
 00 各位好，
 01 今天讲一道我个人觉得很有意思也有代表性的线段计算题
-02 说已知B是AC上一点
+02 说已知点B是线段AC上一点
 03 M是线段AB的中点
 04 N是线段AC的中点
 05 P是线段AN的中点
 06 Q是线段AM的中点
-07 问：BC比PQ的值
-
+07 问：线段BC比PQ的值
+08 这道题的条件有点多
+09 所以画个辅助线帮助记录下各线段的中点
+10 那么，一开始我使用的传统方法
+11 列出线段BC和PQ的长度计算关系
+12 然后找到各个线段中点
+13 通过等长代换，就像这样
+14 算出最后的结果
+15 但这种用线段名置换的计算
+16 很容易在计算过程中笔误导致出错
+17 而且寻找对等关系的过程比较麻烦
+18 所以，还有另一种解题方法
+19 用数轴座标系结合二元一次方程
+20 能够直观、迅速的求解
+21 想知道是怎么做的么？
+23 关注我 下集我会来讲解
 """
         "color": WHITE,
         # A, Q, P, M, N, B,  C
         # 0, 3, 4, 6, 8, 12, 16
-        "a": [-8*0.8, 1, 0],
-        "b": [4*0.8, 1, 0],
-        "c": [8*0.8, 1, 0],
-        "m": [-2*0.8, 1, 0],
-        "n": [0*0.8, 1, 0],
-        "p": [-4*0.8, 1, 0],
-        "q": [-5*0.8, 1, 0],
+        "a": [-8*0.7, 1, 0],
+        "b": [4*0.7, 1, 0],
+        "c": [8*0.7, 1, 0],
+        "m": [-2*0.7, 1, 0],
+        "n": [0*0.7, 1, 0],
+        "p": [-4*0.7, 1, 0],
+        "q": [-5*0.7, 1, 0],
         "txt": 9,
     }
 
@@ -218,21 +230,30 @@ class SegLine2(Scene):
         lAB = Line(self.a, self.b, stroke_width=15, color=RED)
         lAM = Line(self.a, self.m, stroke_width=15, color=BLUE)
         lAN = Line(self.a, self.n, stroke_width=15, color=GREEN)
-        lAP = Line(self.a, self.p, stroke_width=15, color=BLUE)
-        lAQ = Line(self.a, self.q, stroke_width=15, color=GREEN)
+        lNP = Line(self.n, self.p, stroke_width=15, color=BLUE)
+        lMQ = Line(self.m, self.q, stroke_width=15, color=GREEN)
         lPQ = Line(self.p, self.q, stroke_width=15, color=YELLOW)
         lBC = Line(self.b, self.c, stroke_width=15, color=BLUE)
 
-        tx1 = TexMobject("BC=\\frac{1}{3}AC")
+        tx1 = TexMobject("BC").scale(1.5)
         tx1.move_to(self.txt * UP)
-        tx1b = TexMobject("=\\frac{1}{4}BD")
-        tx1b.next_to(tx1, RIGHT)
-        tx2 = TexMobject("AE=EC")
-        tx2.next_to(tx1, DOWN, buff=0.5)
-        tx2b = TexMobject(", BF=FD")
-        tx2b.next_to(tx2, RIGHT)
-        tx3 = TexMobject("EF=5")
-        tx3.next_to(tx2, DOWN, buff=0.5)
+        tx1a = TexMobject("=AC-AB").scale(1.5)
+        tx1a.next_to(tx1, RIGHT)
+        tx2 = TexMobject("PQ").scale(1.5)
+        tx2.next_to(tx1, DOWN, buff=0.8)
+        tx2a = TexMobject("=AP-AQ").scale(1.5)
+        tx2a.next_to(tx2, RIGHT)
+        tx2b = TexMobject("=NP-MQ").scale(1.5)
+        tx2c = TexMobject("=\\frac{1}{2}AN-\\frac{1}{2}AM").scale(1.5)
+        tx2d = TexMobject(
+            "=\\frac{1}{2}\\times(\\frac{1}{2}AC)-\\frac{1}{2}\\times(\\frac{1}{2}AB)").scale(1.5)
+        tx2e = TexMobject("=\\frac{1}{4}(AC-AB)").scale(1.5)
+
+        tx3 = TexMobject("\\frac{BC}{PQ}").scale(1.5)
+        tx3q = TexMobject("=?").scale(1.5)
+        tx3a = TexMobject("=\\frac{AC-AB}{\\frac{1}{4}(AC-AB)}").scale(1.5)
+        tx3b = TexMobject("=\\frac{1}{\\frac{1}{4}}").scale(1.5)
+        tx3c = TexMobject("=4").scale(1.5)
 
         [ptA, ptB, ptC, ptM, ptN, ptP, ptQ] = [
             Dot(X) for X in [self.a, self.b, self.c, self.m, self.n, self.p, self.q]]
@@ -249,18 +270,18 @@ class SegLine2(Scene):
 
         self.play(FadeIn(lALL), FadeIn(ptA), FadeIn(ptC),
                   FadeIn(txtA), FadeIn(txtC))
-        self.wait(3)
+        self.wait(4)
 
         self.play(FadeIn(ptB), FadeIn(txtB))
         self.wait(1)
 
-        self.play(GrowFromCenter(lAC), Indicate(ptN), FadeIn(txtN))
-        self.wait(1)
-        self.remove(lAC)
-
         self.play(GrowFromCenter(lAB), Indicate(ptM), FadeIn(txtM))
         self.wait(1)
         self.remove(lAB)
+
+        self.play(GrowFromCenter(lAC), Indicate(ptN), FadeIn(txtN))
+        self.wait(1)
+        self.remove(lAC)
 
         self.play(GrowFromCenter(lAN), Indicate(ptP), FadeIn(txtP))
         self.wait(1)
@@ -272,7 +293,19 @@ class SegLine2(Scene):
 
         self.play(GrowFromCenter(lBC))
         self.play(GrowFromCenter(lPQ))
-        self.wait(1)
+
+        # 显示问题
+        tx3.move_to((self.txt - 1) * UP)
+        tx3q.next_to(tx3, RIGHT)
+        self.play(TransformFromCopy(lBC, tx3), TransformFromCopy(lPQ, tx3q))
+        vt3x = VGroup(tx3, tx3q)
+        vt3x.generate_target()
+        vt3x.target.shift(LEFT*vt3x.get_center())
+        move3 = MoveToTarget(vt3x)
+        self.play(move3, run_time=0.5)
+        self.wait(2)
+        self.play(FadeOut(vt3x))
+
 
         # 显示标记
         meAC = Measurement(lAC, dashed=True, buff=0.5).add_tips().add_tex(
@@ -284,19 +317,129 @@ class SegLine2(Scene):
         meAM = Measurement(lAM, dashed=True, buff=2.0).add_tips().add_tex(
             "Q'", color=WHITE)
 
-        meAP = Measurement(lAP, dashed=True, buff=-1.5).add_tips().add_tex(
-            "x", color=WHITE)
-        meAQ = Measurement(lAQ, dashed=True, buff=-2.0).add_tips().add_tex(
-            "y", color=WHITE)
-
         self.play(GrowFromCenter(meAC), run_time=0.25)
         self.play(GrowFromCenter(meAB), run_time=0.25)
         self.play(GrowFromCenter(meAN), run_time=0.25)
         self.play(GrowFromCenter(meAM), run_time=0.25)
         self.wait(1)
 
-        self.play(GrowFromCenter(meAP), run_time=0.25)
-        self.play(GrowFromCenter(meAQ), run_time=0.25)
-        self.wait(1)
+        # step 0
+        self.play(FadeIn(tx1))
+        self.play(TransformFromCopy(lBC, tx1a))
+        vt1x = VGroup(tx1, tx1a)
+        vt1x.generate_target()
+        vt1x.target.shift(LEFT*vt1x.get_center())
+        move1 = MoveToTarget(vt1x)
+        self.play(move1, run_time=0.5)
+
+        self.play(FadeIn(tx2))
+        self.play(TransformFromCopy(lPQ, tx2a))
+        vt2x = VGroup(tx2, tx2a)
+        vt2x.generate_target()
+        vt2x.target.shift(LEFT*vt2x.get_center())
+        move2 = MoveToTarget(vt2x)
+        self.play(move2, run_time=0.5)
+        self.wait()
+
+        # step 1, transform
+        tx2b.next_to(tx2, RIGHT)
+        self.play(ReplacementTransform(lNP, tx2a),
+                  ReplacementTransform(lMQ, tx2a))
+        self.play(ReplacementTransform(tx2a, tx2b))
+        self.remove(tx2a)
+        # self.remove(tx2a)
+
+        vt2x = VGroup(tx2, tx2b)
+        vt2x.generate_target()
+        vt2x.target.shift(LEFT*vt2x.get_center())
+        move2 = MoveToTarget(vt2x)
+        self.play(move2, run_time=0.5)
+        self.wait()
+
+        # step 2
+        tx2c.next_to(tx2, RIGHT)
+        self.play(ReplacementTransform(lAN, tx2b),
+                  ReplacementTransform(lAM, tx2b))
+        self.play(ReplacementTransform(tx2b, tx2c))
+        self.remove(tx2b)
+
+        vt2x = VGroup(tx2, tx2c)
+        vt2x.generate_target()
+        vt2x.target.shift(LEFT*vt2x.get_center())
+        move2 = MoveToTarget(vt2x)
+        self.play(move2, run_time=0.5)
+        self.wait()
+
+        # step 3
+        tx2d.next_to(tx2, RIGHT)
+        self.play(ReplacementTransform(lAC, tx2c),
+                  ReplacementTransform(lAB, tx2c))
+        self.play(ReplacementTransform(tx2c, tx2d))
+        self.remove(tx2c)
+
+        vt2x = VGroup(tx2, tx2d)
+        vt2x.generate_target()
+        vt2x.target.shift(LEFT*vt2x.get_center())
+        move2 = MoveToTarget(vt2x)
+        self.play(move2, run_time=0.5)
+        self.wait()
+
+        # step 4
+        tx2e.next_to(tx2, RIGHT)
+        self.play(ReplacementTransform(tx2d, tx2e))
+        self.remove(tx2d)
+
+        vt2x = VGroup(tx2, tx2e)
+        vt2x.generate_target()
+        vt2x.target.shift(LEFT*vt2x.get_center())
+        move2 = MoveToTarget(vt2x)
+        self.play(move2, run_time=0.5)
+        self.wait()
+
+        ind1 = Indicate(tx1, running_start=double_smooth, scale_factor=1.2)
+        ind2 = Indicate(tx2, running_start=double_smooth, scale_factor=1.2)
+        self.play(ind1)
+        self.play(ind2)
+
+        tx3.move_to((self.txt - 1) * UP + 3*LEFT)
+        gx1 = VGroup(tx1, tx2)
+        self.play(ReplacementTransform(gx1, tx3))
+        tx3a.next_to(tx3, RIGHT)
+        gx2 = VGroup(tx1a, tx2e)
+        self.play(ReplacementTransform(gx2, tx3a))
+
+        vt3x = VGroup(tx3, tx3a)
+        vt3x.generate_target()
+        vt3x.target.shift(LEFT*vt3x.get_center())
+        move3 = MoveToTarget(vt3x)
+        self.play(move3, run_time=0.5)
+        self.wait()
+
+
+        tx3b.next_to(tx3, RIGHT)
+        self.play(ReplacementTransform(tx3a, tx3b))
+        vt3x = VGroup(tx3, tx3b)
+        vt3x.generate_target()
+        vt3x.target.shift(LEFT*vt3x.get_center())
+        move3 = MoveToTarget(vt3x)
+        self.play(move3, run_time=0.5)
+        self.wait()
+
+        tx3c.next_to(tx3, RIGHT)
+        self.play(ReplacementTransform(tx3b, tx3c))
+        vt3x = VGroup(tx3, tx3c)
+        vt3x.generate_target()
+        vt3x.target.shift(LEFT*vt3x.get_center())
+        move3 = MoveToTarget(vt3x)
+        self.play(move3, run_time=0.5)
+        self.wait()
+
+        # meAP = Measurement(lAP, dashed=True, buff=-1.5).add_tips().add_tex(
+        #     "x", color=WHITE)
+        # meAQ = Measurement(lAQ, dashed=True, buff=-2.0).add_tips().add_tex(
+        #     "y", color=WHITE)
+        # self.play(GrowFromCenter(meAP), run_time=0.25)
+        # self.play(GrowFromCenter(meAQ), run_time=0.25)
+        # self.wait(1)
 
         self.wait(5)
