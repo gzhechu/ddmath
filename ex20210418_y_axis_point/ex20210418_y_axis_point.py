b0ee7f15317174a468c6812d982d441329543365
gzhechu <gzhechu@gmail.com>
2021-04-20 00:35:58 +0800

    release of point_on_axis

--- ex20210418_y_axis_point/ex20210418_y_axis_point.py
+++ ex20210418_y_axis_point/ex20210418_y_axis_point.py
@@ -26,10 +26,10 @@ class PointOnAxis(Scene):
 05 问：在Y轴上是否存在一个点P
 06 使三角形APC的面积与三角形ABC面积相等
 07 其实这题小学知识就能解
-07 暂停想一想……
-08 现在开始解题：标出AC在Y轴上的交点D
+07 暂停，想一想……
+08 来，开始：标出AC在Y轴上的交点D
 09 三角形APC可以分解为红、蓝色两个小三角形
-10 蓝色三角形面积可用底边PD与蓝色高算出
+10 蓝色三角形面积可用底边PD与蓝色的高算出
 11 同理红色三角形面积用PD与红色高算出
 12 而两个高的长度相加等于AB的长
 13 也就是三角形ABC的一条直角边的长度
@@ -37,9 +37,8 @@ class PointOnAxis(Scene):
 15 可以推理出如果要三角形APC与ABC面积相等
 16 只需满足PD的长度等于BC长度
 16 此时P点对应的位置就本题的答案
-18 具体过程不详细计算，注意有两个答案哦
-19 你学会了么？
-19 好了，下次再见
+18 具体过程不详细计算，注意有两个解哦
+19 你学会了么
     """
 
     def construct(self):
@@ -57,11 +56,12 @@ class PointOnAxis(Scene):
         ptD = Dot(d)
         ptP = Dot(d)
 
-        triangleABC = Polygon(a, c, b, color=WHITE)
+        triangleABC = Polygon(a, c, b, color=WHITE,
+                              fill_opacity=0.2)
         [txtO, txtA, txtB, txtC, txtD, txtP, txtP1] = [
-            Tex(X).scale(0.6) for X in ["O", "A", "B", "C", "D", "P", "P'"]]
+            Tex(X).scale(0.8) for X in ["O", "A", "B", "C", "D", "P", "P'"]]
         [lblA, lblB, lblC, lblD, lblP1, lblP] = [
-            Tex(X).scale(0.6) for X in ["(-2,0)", "(2,0)", "(2,2)", "(1,0)", "(0,3)", "(0,-1)"]]
+            Tex(X).scale(0.8) for X in ["(-2,0)", "(2,0)", "(2,2)", "(1,0)", "(0,3)", "(0,-1)"]]
 
         txtA.next_to(ptA, DL)
         txtB.next_to(ptB, DR)
@@ -86,20 +86,19 @@ class PointOnAxis(Scene):
         self.wait(1)
         self.play(AnimationGroup(FadeOut(vgTxt), FadeIn(vgLbl), lag_ratio=0.5))
         self.wait(1)
-        # self.play(AnimationGroup(Write(triangleABC),  FadeOut(vgLbl),
-        #                          FadeIn(vgTxt), lag_ratio=0.5))
         self.play(Write(triangleABC), FadeOut(vgLbl),
                   FadeIn(vgTxt), run_time=2)
 
         # 画出 P 点
-        self.play(FadeIn(ptP), Indicate(txtP, scale_factor=1.5))
-        triangle = Polygon(a, c, p, color=WHITE)
+        self.play(FadeIn(ptP), Indicate(txtP, scale_factor=1.6))
+        triangle = Polygon(a, c, p, color=WHITE,
+                           fill_opacity=0, stroke_opacity=0)
         g1 = VGroup(triangle, ptP, txtP)
 
         def update1(group, alpha):
             p = [0, 1 + 2 * alpha, 0]
             ptP = Dot(p)
-            txtP = Tex("P").scale(0.6).next_to(ptP, LEFT)
+            txtP = Tex("P").scale(0.8).next_to(ptP, LEFT)
             triangle = Polygon(a, c, p,
                                color=WHITE, fill_opacity=0.2)
             ng = VGroup(triangle, ptP, txtP)
@@ -109,7 +108,7 @@ class PointOnAxis(Scene):
         def update2(group, alpha):
             p = [0, 3 - 4 * alpha, 0]
             ptP = Dot(p)
-            txtP = Tex("P").scale(0.6).next_to(ptP, LEFT)
+            txtP = Tex("P").scale(0.8).next_to(ptP, LEFT)
             triangle = Polygon(a, c, p,
                                color=WHITE, fill_opacity=0.2)
             ng = VGroup(triangle, ptP, txtP)
@@ -121,39 +120,45 @@ class PointOnAxis(Scene):
         self.wait(0.5)
 
         # 出题
-        self.play(Indicate(triangle), run_time=1)
+        self.play(Indicate(triangle, scale_factor=1.1), run_time=1)
         self.wait(0.5)
-        self.play(Indicate(triangleABC), run_time=1)
-        self.wait(2)
+        self.play(Indicate(triangleABC, scale_factor=1.1), run_time=1)
+        self.wait(1)
 
         # 解答
         self.play(UpdateFromAlphaFunc(g1, update2),
-                  run_time=5, rate_func=there_and_back)
-        self.wait(1)
+                  run_time=3, rate_func=there_and_back)
+        self.wait(2)
+        axes = Axes(axis_config={"include_tip": True, "include_ticks": True})
+        self.add(axes)
+        self.play(FadeOut(numberPlane), run_time=3)
+
         p = [0, 3, 0]
-        self.play(FadeIn(ptD), Indicate(txtD, scale_factor=1.5))
+        self.play(Indicate(ptD, scale_factor=1.6),
+                  Indicate(txtD, scale_factor=1.6))
         self.wait(1)
         t1 = Polygon(a, p, d, color=RED,  fill_opacity=0.5)
         t2 = Polygon(c, p, d, color=BLUE, fill_opacity=0.5)
         self.play(FadeIn(t1), FadeIn(t2))
-        # self.play(Indicate(t2))
-        self.play(FadeOut(t1), FadeOut(t2))
         self.wait(1)
+        self.play(FadeOut(t1), FadeOut(t2))
 
-        lPD = DashedLine(p, d, stroke_width=10, color=WHITE)
-        lCE = DashedLine(c, e, stroke_width=10, color=BLUE)
-        lAO = DashedLine(a, ORIGIN, stroke_width=10, color=RED)
+        lPD = Line(p, d, stroke_width=10, color=WHITE)
+        lCE = Line(c, e, stroke_width=10, color=BLUE)
+        lAO = Line(a, ORIGIN, stroke_width=10, color=RED)
+        lAB = Line(a, b, stroke_width=10, color=RED)
         # 第一个小三角形
         self.play(FadeIn(t2))
         self.wait(0.5)
-        self.play(GrowFromCenter(lPD), FadeOut(t2))
+        self.play(GrowFromCenter(lPD), FadeOut(t2),
+                  Indicate(txtP, scale_factor=1.6), Indicate(txtD, scale_factor=1.6))
         self.play(ShowCreation(lCE))
         self.wait(1)
 
         # 第二个小三角形
         self.play(FadeIn(t1))
-        self.wait(0.5)
-        self.play(WiggleOutThenIn(lPD), FadeOut(t1))
+        self.play(WiggleOutThenIn(lPD),  FadeOut(t1),
+                  Indicate(txtP, scale_factor=1.6), Indicate(txtD, scale_factor=1.6))
         self.play(ShowCreation(lAO))
         self.wait(1)
 
@@ -163,28 +168,33 @@ class PointOnAxis(Scene):
         move1 = MoveToTarget(lCE)
         self.play(move1)
         self.wait(1)
-        self.play(WiggleOutThenIn(lAO), WiggleOutThenIn(lCE))
-        self.play(FadeOut(lPD), FadeOut(lCE), FadeOut(lAO))
-        self.wait(2)
+        # 抖动底边 AB
+        self.remove(lAO, lCE)
+        self.play(ApplyWave(lAB))
+        self.play(FadeOut(lAB), Indicate(
+            triangleABC, scale_factor=1.1), run_time=2)
+        self.wait(3)
 
         # 标记三角形
-        self.play(Indicate(triangle), run_time=1)
-        self.play(Indicate(triangleABC), run_time=1)
+        self.play(Indicate(triangle, scale_factor=1.1), run_time=1)
+        self.play(Indicate(triangleABC, scale_factor=1.1), run_time=1)
 
         # 平行移动底边
+        self.wait(1)
         self.play(WiggleOutThenIn(lPD))
-        self.wait(0.5)
         lPD.generate_target()
         lPD.target.shift([2, -1, 0])
         move1 = MoveToTarget(lPD)
         self.play(move1)
+        self.wait(1)
 
         lPD.generate_target()
         lPD.target.shift([-2, 1, 0])
-        move1 = MoveToTarget(lPD)
-        self.play(move1)
+        move2 = MoveToTarget(lPD)
+        self.play(move2, Indicate(txtP, scale_factor=1.6))  # 显示 P点
         # self.wait(1)
         self.play(FadeOut(lPD))
+        self.wait(1)
 
         # 复制第一个点
         ptP1 = ptP.copy()
@@ -192,18 +202,15 @@ class PointOnAxis(Scene):
         triangle1 = triangle.copy()
         triangle1.set_fill(color=BLUE, opacity=0.3)
         self.add(triangle1, ptP1, txtP1)
-        self.wait(2)
 
         # 移动到第二个点
         txtP.next_to(ptP, LEFT)
         g1 = VGroup(triangle, ptP, txtP)
         self.play(UpdateFromAlphaFunc(g1, update2),
-                  run_time=2, rate_func=smooth)
-
-        self.play(FadeOut(triangle1))
-        self.play(FadeOut(triangle))
+                  run_time=1, rate_func=smooth)
 
         lblP.next_to(ptP, RIGHT)
         lblP1.next_to(ptP1, RIGHT)
-        self.play(FadeIn(lblP), FadeIn(lblP1))
-        self.wait(1)
+        self.play(FadeIn(lblP), FadeIn(lblP1),
+                  FadeOut(triangle1), FadeOut(triangle))
+        self.wait(2)
