52bf6db1a13ab62415c8d0ae0cb3da5f428eb991
Robin Ho <hechu@qq.com>
2021-05-20 00:01:07 +0800

    backup

--- ex20210518_moving_triangle/ex20210518_moving_triangle.py
+++ ex20210518_moving_triangle/ex20210518_moving_triangle.py
@@ -42,8 +42,7 @@ class MovingTriangle(MovingCameraScene):
         [txtO, txtA, txtB, txtC, txtD, txtE, txtF, txtG, txtH] = [
             Tex(X) for X in ["O", "A", "B", "C", "D", "E", "F", "G", "H"]]
 
-        [txt1, txt2, txt3, txt4, txt5, txt6] = [
-            Tex(X) for X in ["1", "2", "3", "4", "5", "6"]]
+        [lblB, lblC] = [Tex(X) for X in ["(0,7)", "(24,7)"]]
 
         txtO.next_to(ptO, DL)
         txtA.next_to(ptA, DR)
@@ -53,9 +52,13 @@ class MovingTriangle(MovingCameraScene):
         txtE.next_to(ptE, DR)
         txtF.next_to(ptF, DR)
 
+        lblB.next_to(ptB, DR)
+        lblC.next_to(ptC, DR)
+
         ptTxts = [txtA, txtB, txtC]
         vgTxts = VGroup(*ptTxts)
         vgPt = VGroup(ptA, ptB, ptC)
+        vgLbls = VGroup(lblB, lblC)
 
         axes = Axes(axis_config={"include_tip": True, "include_ticks": False},
                     x_min=-2, x_max=33, y_min=-2, y_max=13,
@@ -69,6 +72,21 @@ class MovingTriangle(MovingCameraScene):
         # 显示顶点
         self.play(FadeIn(vgPt))
         self.play(FadeIn(vgTxts))
+        self.wait()
+        self.play(FadeIn(vgLbls))
+        self.wait()
+
+        tx1 = Tex("B=(0,7), C=(24,7)").scale(1.5)
+        tx2 = MathTex("V_E:V_D=3:2").scale(1.5)
+        tx3 = MathTex(
+            "S_{\\bigtriangleup OEF}", ">", "S_{\\bigtriangleup CDF}", color=RED).scale(1.5)
+        tx3.set_color_by_tex('OEF', RED)
+        tx3.set_color_by_tex('>', WHITE)
+        tx3.set_color_by_tex('CDF', BLUE)
+        tx1.move_to(UP * 9 + center)
+        tx2.next_to(tx1, DOWN, buff=0.5)
+        tx3.next_to(tx2, DOWN, buff=0.5)
+        self.play(ReplacementTransform(vgLbls, tx1))
         self.wait()
 
         lOC = Line(ORIGIN, c)
@@ -96,8 +114,10 @@ class MovingTriangle(MovingCameraScene):
             return group
 
         self.play(UpdateFromAlphaFunc(g1, update1), FadeIn(ptF.copy()),
-                  FadeIn(txtF.copy()), run_time=4, rate_func=there_and_back)
-        self.remove(lDE)
+                  FadeIn(txtF.copy()), run_time=4, rate_func=linear)
+        self.wait()
+        # self.remove(lDE)
+        self.play(ReplacementTransform(lDE, tx2))
         self.wait()
 
         def update2(group, alpha):
@@ -116,7 +136,7 @@ class MovingTriangle(MovingCameraScene):
         self.add(ptF, txtF)
         self.play(UpdateFromAlphaFunc(g1, update2),
                   run_time=2, rate_func=smooth)
-        self.wait()
+        self.wait(3)
 
         d1 = d + 4.5 * unit_size * LEFT * 2
         e1 = e + 4.5 * unit_size * RIGHT * 3
@@ -124,5 +144,11 @@ class MovingTriangle(MovingCameraScene):
         tri2 = Polygon(o, e1, f, color=RED, fill_opacity=0.3)
         poly1 = Polygon(o, b, d1, f, color=WHITE, fill_opacity=0.3)
         self.play(Create(tri1), Create(tri2))
-        self.play(Create(poly1))
+        self.wait()
+
+        vgTriangle = VGroup(tri1, tri2)
+        self.play(TransformFromCopy(vgTriangle, tx3))
+        self.wait()
+
+        # self.play(Create(poly1))
         self.wait(5)
